cb033f59815b7f75a69c79fa6b93d3773d3a532a
saurabh <saurabhfarkade83@gmail.com>
2024-06-04 00:16:06 +0530

    First commit- basic project setup completed

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const { PORT } = require("./config/serverConfig");
+const bodyParser = require("body-parser");
+
+const setupAndStartServer = async () => {
+  const app = express();
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+
+  app.listen(PORT, async () => {
+    console.log(`server running at ${PORT}`);
+  });
+};
+setupAndStartServer();
