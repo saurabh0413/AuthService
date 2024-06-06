b13354fce989e9fc7e48361901fc910d73c878c4
saurabh <saurabhfarkade83@gmail.com>
2024-06-06 17:17:09 +0530

    Added signup password encryption

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const { PORT } = require("./config/serverConfig");
+const apiRoutes = require("./routes/index");
 const bodyParser = require("body-parser");
-
+const app = express();
 const setupAndStartServer = async () => {
-  const app = express();
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
-
+  app.use("/api", apiRoutes);
   app.listen(PORT, async () => {
     console.log(`server running at ${PORT}`);
   });
