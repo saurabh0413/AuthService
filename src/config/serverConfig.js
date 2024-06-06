f8bdd8195538fc06580576252ac41acd1d331146
saurabh <saurabhfarkade83@gmail.com>
2024-06-06 17:32:25 +0530

    Added functions to create token and verify token

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -1,8 +1,9 @@
 const dotenv = require("dotenv");
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 dotenv.config();
 
 module.exports = {
   PORT: process.env.PORT,
-  SALT: bcrypt.genSaltSync(9)
+  SALT: bcrypt.genSaltSync(9),
+  JWT_KEY: process.env.JWT_KEY,
 };
