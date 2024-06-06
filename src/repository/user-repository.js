1b3751b40ba6a1d3a2ce5b0663bd635e18675e59
saurabh <saurabhfarkade83@gmail.com>
2024-06-06 17:27:03 +0530

    Added get by id and email in repository

--- src/repository/user-repository.js
+++ src/repository/user-repository.js
@@ -24,6 +24,30 @@ class UserRepository {
       throw error;
     }
   }
+  async getById(userId) {
+    try {
+      const user = await User.findByPk(userId, {
+        attributes: ["email", "id"],
+      });
+      return user;
+    } catch (error) {
+      console.log("Something went wrong on repository");
+      throw error;
+    }
+  }
+  async getByEmail(userEmail) {
+    try {
+      const user = await User.findOne({
+        where: {
+          email: userEmail,
+        },
+      });
+      return user;
+    } catch (error) {
+      console.log("Something went wrong on repository");
+      throw error;
+    }
+  }
 }
 
 module.exports = UserRepository;
